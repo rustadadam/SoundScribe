25d24e9af2b0941ac914cd440b4a5117868f1fe0
rustadadam <rustadadam@gmail.com>
2025-04-01 11:12:23 -0600

    Working files

--- model/book_nlp.py
+++ model/book_nlp.py
@@ -16,8 +16,8 @@ booknlp = BookNLP("en", model_params)
 
 # Define input and output paths
 input_file = "/yunity/arusty/SoundScribe/text_files/chapter4.txt"
-output_dir = "/yunity/arusty/SoundScribe/text_files/book_files"
-book_id = "lotl_chapter4"
+output_dir = "/yunity/arusty/SoundScribe/book_files"
+book_id = "lotl-chapter4"
 
 # Process the book
 booknlp.process(input_file, output_dir, book_id)
